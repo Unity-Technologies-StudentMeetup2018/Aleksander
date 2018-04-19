eef9900c8551d5f64f1b14078012a4dc5127bb2c
Fjellvang <drk441@alumni.ku.dk>
2018-04-19 19:53:43 +0200

    First commit

---
+++ CSharp/src/Map.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Text;
+using System.Collections;
+
+namespace PathfindingCSharp
+{
+    public struct Vector2 : IEquatable<Vector2>
+    {
+        public int x, y;
+
+        public bool Equals(Vector2 other)
+        {
+            return x == other.x && y == other.y;
+        }
+    }
+    public class Map
+    {
+        public const char emptyChar = '.';
+        public const char blockedChar = '#';
+        public const char invalidChar = '?';
+        public const char pathChar = '@';
+
+        private char[,] mapPositions;
+        Vector2 startPosition;
+        Vector2 goalPosition;
+
+        public Map(string[] mapData, Vector2 startPos, Vector2 endPos)
+        {
+            if (startPos.x < 0 || startPos.x >= mapData.Length || startPos.y < 0 || startPos.y >= mapData.Length)
+                throw new Exception("Start position is out of bounds, provided coordinates were: " + startPos.x + "," + startPos.y);
+
+            if (endPos.x < 0 || endPos.x >= mapData.Length || endPos.y < 0 || endPos.y >= mapData.Length)
+                throw new Exception("End position is out of bounds, provided coordinates were: " + endPos.x + "," + endPos.y);
+
+            if (mapData[startPos.y][startPos.x] == blockedChar)
+                throw new Exception("Start position is invalid, provided coordinates were: " + startPos.x + "," + startPos.y);
+
+            if (mapData[endPos.y][endPos.x] == blockedChar)
+                throw new Exception("End position is invalid, provided coordinates were: " + endPos.x + "," + endPos.y);
+
+            startPosition = startPos;
+            goalPosition = endPos;
+
+            mapPositions = new char[mapData.Length, mapData.Length];
+
+            for (int i = 0; i < mapData.Length; ++i)
+            {
+                for (int j = 0; j < mapData[i].Length; ++j)
+                {
+                    int index = (i * mapData.Length) + j;
+
+                    if (startPos.x == j && startPos.y == i)
+                        mapPositions[i, j] = pathChar;
+                    else if (endPos.x == j && endPos.y == i)
+                        mapPositions[i, j] = pathChar;
+                    else
+                    {
+                        switch (mapData[i][j])
+                        {
+                            case emptyChar:
+                                mapPositions[i, j] = emptyChar;
+                                break;
+
+                            case blockedChar:
+                                mapPositions[i, j] = blockedChar;
+                                break;
+
+                            default:
+                                mapPositions[i, j] = invalidChar;
+                                break;
+                        }
+                    }
+
+                }
+            }
+        }
+
+        private bool IsBlocked(Vector2 position)
+        {
+            return mapPositions[position.y, position.x] == blockedChar;
+        }
+
+        public void DisplayMap()
+        {
+            Console.Clear();
+            Console.WriteLine(SolutionToString());
+        }
+
+        public string SolutionToString()
+        {
+            StringBuilder sb = new StringBuilder(mapPositions.Length + mapPositions.GetLength(0)); //add extra space for the new lines
+
+            for (int i = 0; i < mapPositions.GetLength(0); ++i)
+            {
+                for (int j = 0; j < mapPositions.GetLength(1); ++j)
+                {
+                    sb.Append(mapPositions[i, j]);
+                }
+                sb.Append('\n');
+            }
+
+            return sb.ToString();
+        }
+
+        public void ComputePath()
+        {
+            //TODO: Implement solution here
+            var currentPos = startPosition;
+            var bounds = mapPositions.Length;
+            while (currentPos != goalPosition)
+            {
+                
+            }
+        }
+    };
+
+    public struct Point
+    {
+        Vector2 cameFrom;
+        bool visited;
+    }
+}
---
+++ README.md
@@ -0,0 +1,38 @@
+# PathfindingProblem
+
+Code challenge april 2018.
+
+Task:
+- Implement 2D grid based pathfinding.
+- Choose whether you want to do so in C++, C# or both
+
+For C++
+- in Cpp/src/nap.cpp
+- implement Map::ComputePath() 
+
+For C#
+- in CSharp/src/Map.cs
+- implement Map.ComputePath()
+
+Input Data:
+- Static 32x32 Grid as text file
+- '#' character means tile is blocked
+- '.' character means tile is free
+- The supplied grid file file can be assumed to be valid
+
+Execution:
+- pathfind.exe <Relative Map Path> <startX> <startY> <endX> <endY>
+- e.g.: 
+-        pathfind.exe map0.txt 16 5 16 26
+
+Expected result:
+- 8 directional movement.
+- Prints map to console with marked found path (@). 
+- Application terminates 
+
+
+*Test Maps:*
+The Maps folder contains 4 test maps, each which have the following start positions:
+- Maps/test0.txt 16 5 16 26
+
+
